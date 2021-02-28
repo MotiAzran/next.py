6246ac337f798f940af7f356e6ee0a6efa31f0e4
Moti Azran <motiazran9@gmail.com>
2021-02-28 21:08:36 +0200

    Fix exerecise 1.3.4

--- 1-3-4.py
+++ 1-3-4.py
@@ -1,7 +1,6 @@
 import string
 
 password = "sljmai ugrf rfc ambc: lglc dmsp mlc rum"
-to = ''.join([chr(((ord(c) - ord('a') + 2) % 26) + ord('a')) for c in string.ascii_lowercase])
 
 # print(''.join([chr(((ord(c) - ord('a') + 2) % 26) + ord('a')) if c.isalpha() else c for c in password]))
-print(password.translate(string.maketrans(string.ascii_lowercase, to)))
+print(password.translate({ord(c): (((ord(c) - ord('a') + 2) % 26) + ord('a')) for c in string.ascii_lowercase}))
